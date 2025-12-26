9d6d8d03532d7e23b787d2757f253f4be884ba3d
xianyu-dama <xianyu-dama@gmail.com>
2025-12-26 12:21:18 +0800

    fix

--- start/src/test/java/com/xianyu/architecture/ModulithArchitectureUnitTest.java
+++ start/src/test/java/com/xianyu/architecture/ModulithArchitectureUnitTest.java
@@ -1,14 +1,22 @@
 package com.xianyu.architecture;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.xianyu.Application;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.modulith.core.ApplicationModules;
 
 class ModulithArchitectureUnitTest {
 
     @Test
     void verifyModularity() {
-        var modules = ApplicationModules.of(Application.class);
+        var modules = ApplicationModules.of(
+                Application.class,
+                JavaClass.Predicates.resideInAPackage("com.xianyu.component..")
+                        .or(JavaClass.Predicates.resideInAnyPackage("com.xianyu.config.."))
+        );
+
         modules.verify();
     }
+
 }
